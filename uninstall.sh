02cf11df78028b495ad73356209276d9407232f1
entrhopi <alexander@freynik.org>
2023-08-22 13:51:41 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise $SERVICE_NAME')
+kill $(ps | grep "supervise $SERVICE_NAME" | grep -wv 'grep\|vi\|vim' | awk '{print $1}')
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
